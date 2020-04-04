a01748c7424f1d56240eeca62055d70b44bde77e
ameddin73 <ameddin73@gmail.com>
2020-04-04 14:51:07 -0400

    add collision and refine spawn

---
+++ Assets/Scripts/DeployAsteroid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployAsteroid : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+
+    public float respawnTime = 1.0f;
+    private Vector2 _screenBounds;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _screenBounds =
+            Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+        StartCoroutine(AsteroidWave());
+    }
+
+    private void SpawnAsteroid()
+    {
+        GameObject newAsteroid = Instantiate(asteroidPrefab) as GameObject;
+        float xSpawnRange = (float) (_screenBounds.x * 1.5);
+        float ySpawnRange = (float) (_screenBounds.y * 1.5);
+        newAsteroid.transform.position =
+            new Vector2(Position(xSpawnRange), Position(ySpawnRange));
+        Debug.Log(newAsteroid.transform.position);
+    }
+
+    private float Position(float spawnRange)
+    {
+        return Random.value > 0.5f ? Random.Range(_screenBounds.x, spawnRange) : Random.Range(-spawnRange, 0);
+    }
+
+    IEnumerator AsteroidWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            SpawnAsteroid();
+        }
+    }
+}
--- Assets/Scripts/asteroid.cs
+++ Assets/Scripts/asteroid.cs
@@ -2,25 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class asteroid : MonoBehaviour
+public class Asteroid : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float minSpeed = 1.0f;
+    public float maxSpeed = 10.0f;
     private Rigidbody2D _rigidbody2D;
     private Vector2 _screenBounds;
     
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody2D = this.GetComponent<Rigidbody2D>();
-        _rigidbody2D.velocity = new Vector2(-speed, 0);
+        // generate screenbounds for interactions
         _screenBounds =
             Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+        
+        // Set initial velocity
+        _rigidbody2D = this.GetComponent<Rigidbody2D>();
+        float speed = Random.Range(minSpeed, maxSpeed);
+        float xVelocity = Random.Range(0, speed);
+        float yVelocity = speed - Mathf.Abs(xVelocity);
+        if (transform.position.x > 0)
+        {
+            xVelocity = -xVelocity;
+        }
+        if (transform.position.y > 0)
+        {
+            yVelocity = -yVelocity;
+        }
+        _rigidbody2D.velocity = new Vector2(xVelocity, yVelocity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < _screenBounds.x * 2)
+        if (transform.position.x < _screenBounds.x * 2 ||
+            transform.position.x > -_screenBounds.x * 2 ||
+            transform.position.y < _screenBounds.y * 2 ||
+            transform.position.y > -_screenBounds.y * 2)
         {
             Destroy(this.gameObject);
         }
