c9c58ac196f4a18c992f69038fca83ec432cd019
ameddin73 <ameddin73@gmail.com>
2020-04-04 20:03:35 -0400

    pretty much get it together on collisions and stuff

--- Assets/Scripts/asteroid.cs
+++ Assets/Scripts/asteroid.cs
@@ -1,26 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Asteroid : MonoBehaviour
 {
     public float minSpeed = 1.0f;
     public float maxSpeed = 10.0f;
+    public float slowSpeed = 0.01f;
+    public float massMultiplier = 100f;
+    private Collider _collider;
     private Rigidbody2D _rigidbody2D;
     private Vector2 _screenBounds;
+    private float _startSpeed;
+    private float _startBounce;
     
     // Start is called before the first frame update
     void Start()
     {
+        _collider = GetComponent<Collider>();
         // generate screenbounds for interactions
         _screenBounds =
             Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Camera.main.nearClipPlane));
         
         // Set initial velocity
         _rigidbody2D = this.GetComponent<Rigidbody2D>();
-        float speed = Random.Range(minSpeed, maxSpeed);
-        float xVelocity = Random.Range(0, speed);
-        float yVelocity = speed - Mathf.Abs(xVelocity);
+        _startSpeed = Random.Range(minSpeed, maxSpeed);
+        float xVelocity = Random.Range(0, _startSpeed);
+        float yVelocity = _startSpeed - Mathf.Abs(xVelocity);
         if (transform.position.x > 0)
         {
             xVelocity = -xVelocity;
@@ -33,7 +41,27 @@ public class Asteroid : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        TestBounds();
+    }
+
+    private void OnMouseDown()
+    {
+        _rigidbody2D.velocity *= slowSpeed;
+        _rigidbody2D.mass *= massMultiplier;
+        _startBounce = _collider.material.bounciness;
+        _collider.material.bounciness = 0;
+    }
+
+    private void OnMouseUp()
+    {
+        _rigidbody2D.velocity *= _startSpeed / _rigidbody2D.velocity.magnitude;
+        _rigidbody2D.mass /= massMultiplier;
+        _collider.material.bounciness = _startBounce;
+    }
+
+    private void TestBounds()
     {
         if (transform.position.x > _screenBounds.x * 2 ||
             transform.position.x < -_screenBounds.x * 2 ||
