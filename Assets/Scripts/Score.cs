363957e777bd03c0e34b642e33d3ba259edfaeca
ameddin73 <ameddin73@gmail.com>
2020-04-06 21:48:56 -0400

    fudge ui and add escape button to quit

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,10 +5,9 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-
     private int _score, _highScore, _delta;
     public Text scoreObject, highScoreObject;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,18 +22,20 @@ public class Score : MonoBehaviour
     public void EndGame()
     {
         _highScore = Mathf.Max(_score, _highScore);
-        highScoreObject.GetComponent<Text>().text = ""+_highScore;
+        highScoreObject.GetComponent<Text>().text = "" + _highScore;
         _score = _delta = 0;
-        scoreObject.GetComponent<Text>().text = ""+_score;
+        scoreObject.GetComponent<Text>().text = "" + _score;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
         if (_score != _delta)
         {
             _delta = _score;
-            scoreObject.GetComponent<Text>().text = ""+_score;
+            scoreObject.GetComponent<Text>().text = "" + _score;
         }
     }
 }
