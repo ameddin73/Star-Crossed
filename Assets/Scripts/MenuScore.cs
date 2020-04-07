6f7d7c2d2213c247ebd7502b605de7e9788187f6
ameddin73 <ameddin73@gmail.com>
2020-04-07 17:59:29 -0400

    added home menu and ui + updated unity version

---
+++ Assets/Scripts/MenuScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScore : MonoBehaviour
+{
+
+    public Text highScoreObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        highScoreObject.GetComponent<Text>().text = "" + PlayerPrefs.GetInt("highScore");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // highScoreObject.GetComponent<Text>().text = "" + PlayerPrefs.GetInt("highScore");
+    }
+}
